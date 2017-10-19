fd6d0e1a898239d44c9e16f5cbd763b8665e6958
tjallen27 <tjallen91@hotmail.co.uk>
2017-10-19 10:46:23 +0100

    Static home and about added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'static/home'
+
+  get 'static/about'
+
   resources :recipes
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static#home'
+  get '/about', to: 'static#about'
 end
